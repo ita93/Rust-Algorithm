b8eba3d1ee1d9bf0b0e28f22f6d1ab2e9eda4259
Nguyen Dinh Phi <phind.uet@gmail.com>
2019-03-21 17:56:45 +0800

    Tour of the Knight

---
+++ knight_tour/src/main.rs
@@ -0,0 +1,100 @@
+use std::fmt;
+
+const SIZE: usize = 8;
+const MOVES: [(i32, i32);8] = [(2,1),(1,2),(-1,2),(-2,1),(-2,-1),(-1,-2),(1,-2),(2,-1)];
+
+#[derive(Copy, Clone, Eq, PartialEq, PartialOrd, Ord)]
+struct Point{
+    x: i32,
+    y: i32,
+}
+
+impl Point{
+    fn mov(&self, &(dx, dy): &(i32, i32)) -> Self{
+        Point{
+            x: self.x + dx,
+            y: self.y + dy,
+        }
+    }
+}
+
+struct Board{
+    field: [[i32; SIZE]; SIZE]
+}
+
+impl Board{
+    fn new() -> Board{
+        Board{
+            field: [[0; SIZE]; SIZE]
+        }
+    }
+
+    fn available(&self, p: Point) -> bool {
+        //Check if given point is inside the board.
+        let valid = 0 <= p.x && p.x < SIZE as i32
+            && 0 <= p.y && p.y < SIZE as i32;
+        //Check if given point was visited or not?
+        return valid && self.field[p.x as usize][p.y as usize] == 0;
+    }
+
+    //calculate the number of possible moves
+    fn count_degree(&self, p: Point) -> i32{
+        let mut count = 0;
+        for dir in MOVES.iter(){
+            let next = p.mov(dir);
+            if self.available(next){
+                count += 1;
+            }
+        }
+        count
+    }
+}
+
+impl fmt::Display for Board{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+        for row in self.field.iter(){
+            for x in row.iter(){
+                write!(f, "{:3}", x);
+            }
+            write!(f, "\n");
+        }
+        Ok(())
+    }
+}
+
+fn knights_tour(x: i32, y: i32) -> Option<Board>{
+    let mut board = Board::new();
+    let mut p = Point{x, y};
+    let mut step = 1;
+    board.field[p.x as usize][p.y as usize] = step;
+    step += 1;
+
+    while step <= (SIZE * SIZE) as i32{
+        let mut candidates = Vec::new();
+        for dir in MOVES.iter(){
+            let adj = p.mov(dir);
+            if board.available(adj){
+                let degree = board.count_degree(adj);
+                candidates.push((degree, adj));
+            }
+        }
+
+        match candidates.iter().min(){
+            Some(&(_, adj)) => p = adj,
+            None => return None,
+        };
+        board.field[p.x as usize][p.y as usize] = step;
+        step += 1;
+    }
+    Some(board)
+}
+
+fn main() {
+    let (x, y) = (3,1);
+    println!("Board size: {}", SIZE);
+    println!("Starting position: ({}, {})", x, y);
+    match knights_tour(x, y){
+        Some(b) => println!("{}", b),
+        None => println!("Fail!"),
+    }
+}
