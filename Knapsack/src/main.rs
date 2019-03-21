0c68a427ea1803e3dd41b212c47fdf2659ddab7c
Phi Nguyen <phind.uet@gmail.com>
2019-03-21 23:32:44 +0800

    Use tuple

--- Knapsack/src/main.rs
+++ Knapsack/src/main.rs
@@ -1,24 +1,21 @@
 /*
  * Input: 
- *  v: list of values (of items)
- *  w: list of weights (of items)
+ *  items: list of items (value, weight)
  *  limit: The max weight that the bag can be.
  */
-fn knapsack(v: &Vec<u32>, w: &Vec<u32>, limit: usize) -> u32{
-    if v.len() != w.len() {
-        panic!("W and V must be the same size");
-    }
-    let mut mem_table = vec![vec![0;limit+1]; v.len()+1];
-    for i in 1..=v.len(){
+fn knapsack(items: &Vec<(u32, u32)>, limit: usize) -> u32{
+    let mut mem_table = vec![vec![0;limit+1]; items.len()+1];
+    for i in 1..=items.len(){
         for j in 1..=limit{
-            if j < w[i-1] as usize {
+            if j < items[i-1].1 as usize { //items[i-1].1
                 mem_table[i][j] = mem_table[i][j]; //J < w[i-1] so we cannot take i.
             } else {
-                mem_table[i][j] = max_of_two(mem_table[i-1][j], mem_table[i-1][ j - w[i-1] as usize] + v[i-1]);
+                mem_table[i][j] = max_of_two(mem_table[i-1][j], mem_table[i-1][ j - items[i-1].1 as usize] + items[i-1].0);
+                //
             }
         }
     }
-    mem_table[v.len()][limit]
+    mem_table[items.len()][limit]
 }
 
 fn max_of_two(a: u32, b: u32) -> u32{
@@ -30,7 +27,6 @@ fn max_of_two(a: u32, b: u32) -> u32{
 
 #[test]
 fn test_first_case() {
-    let values = vec![60, 100, 120];
-    let weights = vec![10, 20, 30];
-    assert!(knapsack(&values, &weights, 50) == 220);
+    let items = vec![(60, 10), (100, 20), (120, 30)];
+    assert!(knapsack(&items, 50) == 220);
 }
