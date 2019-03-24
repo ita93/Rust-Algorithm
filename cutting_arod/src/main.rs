1fae0eca50a62bf326f1e4cbf22bedb82afb34a9
Phi Nguyen <phind.uet@gmail.com>
2019-03-24 14:32:51 +0800

    Cutting a rod solution

--- cutting_arod/src/main.rs
+++ cutting_arod/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+fn cut_rod(input: &Vec<u32>) -> u32{
+    let mut val = vec![0; input.len() + 1];
+    for i in 1..=input.len(){
+        let mut max_val = 0;
+        for j in 0..i {
+            max_val = std::cmp::max(max_val, input[j] + val[i-j-1]); 
+        }
+        val[i] = max_val;
+    }
+    val[input.len()]
+}
+
+#[test]
+fn test_case_1() {
+    let input = vec![1,5,7,9,10,17,17,20];
+    assert_eq!(cut_rod(&input), 22);
+}
+
+#[test]
+fn test_case_2() {
+    let input = vec![3, 5, 8, 9, 10, 17, 17, 20];
+    assert_eq!(cut_rod(&input), 24);
 }
